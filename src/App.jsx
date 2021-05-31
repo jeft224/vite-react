6ce50d9deb7d47bcf795011ce1f39b47ff12b15c
abert <zhiwei.luo@zuolin.com>
2021-05-31 11:33:55 +0800

    chore([.postcssrc.js]): 移除高清方案的实现，配置存在问题

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
-import {
-	HashRouter as Router,
-	Route,
-	Redirect,
-	Switch
-} from 'react-router-dom';
-
-import { Button, DatePicker, List } from 'antd-mobile';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import routes from './routes';
 
 function App(props) {
-	const [count, setCount] = useState(0);
-	console.log(props);
-	const date = new Date();
 	return (
 		<div className="App">
 			<Router>
 				<Route exact path="/home" component={Home} />
 				<Route path="/about" component={About} />
+				<Redirect to={'/home'} from="/"></Redirect>
 			</Router>
 		</div>
 	);
