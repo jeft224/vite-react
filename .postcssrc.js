275b13a9534d2feb2ba5671ad0851d8f422d9e22
abert <zhiwei.luo@zuolin.com>
2021-05-31 17:44:41 +0800

    chore(postcssrc.js): 验证移动端高清方案的正确性

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,18 +1,27 @@
-// module.exports = {
-//   plugins: {
-//     'postcss-flexbugs-fixes': {},
-//     'postcss-import': {}, // 解决@import引入路径问题
-//     'postcss-url': {}, // 该插件主要用来处理文件，比如图片文件、字体文件等引用路径的处理.
-//     // 'postcss-border-1px': {}, // 移动端1px解决方案
-//     'postcss-aspect-ratio-mini': {}, // 处理元素容器
-//     'postcss-px-to-viewport': {
-//       viewportWidth: 828, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750.
-//       viewportHeight: 1792, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
-//       unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-//       viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
-//       selectorBlackList: ['.ignore'], //指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名。
-//       minPixelValue: 0.5, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-//       mediaQuery: false // 允许在媒体查询中转换`px`
-//     }
-//   }
-// };
+module.exports = {
+	plugins: {
+    'postcss-preset-env':{
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    },
+		'postcss-flexbugs-fixes': {},
+		'postcss-import': {}, // 解决@import引入路径问题
+		'postcss-url': {}, // 该插件主要用来处理文件，比如图片文件、字体文件等引用路径的处理.
+		// 'postcss-border-1px': {}, // 移动端1px解决方案
+		'postcss-aspect-ratio-mini': {}, // 处理元素容器
+		'postcss-px-to-viewport': {
+			unitToConvert: 'px',
+			viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750.
+			viewportHeight: 1334, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
+			unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+			viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+			fontViewportUnit: 'vw',
+			selectorBlackList: ['.ignore'], //指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名。
+			minPixelValue:1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+			mediaQuery: false, // 允许在媒体查询中转换`px`
+      exclude: /node_modules/i
+		}
+	}
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, DatePicker, List } from 'antd-mobile';
+import './index.less';
 
 export default function Home(props) {
 	const date = new Date();
@@ -7,6 +8,8 @@ export default function Home(props) {
 	return (
 		<div>
 			<h3>Home</h3>
+			<div className="test-viewport">测试转换</div>
+
 			<List>
 				<DatePicker value={date}>
 					<List.Item arrow="horizontal">Datetime</List.Item>
